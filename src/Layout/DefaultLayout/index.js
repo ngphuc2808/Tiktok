1bee2cc69b30331e5e6a835d562dbfeb68a382e4
ngphuc2808 <ngphuc.2808@gmail.com>
2023-01-08 18:11:01 +0700

    Setup Layout

--- src/Layout/DefaultLayout/index.js
+++ src/Layout/DefaultLayout/index.js
@@ -1,12 +1,17 @@
 import Header from '~/Layout/components/Header';
 import Sidebar from './Sidebar';
+import classNames from 'classnames/bind';
+import styles from './DefaultLayout.module.scss';
 
+const cx = classNames.bind(styles);
 function DefaultLayout({ children }) {
   return (
-    <div>
+    <div className={cx('wrapper')}>
       <Header />
-      <Sidebar />
-      <div>{children}</div>
+      <div className={cx('container')}>
+        <Sidebar />
+        <div className={cx('content')}>{children}</div>
+      </div>
     </div>
   );
 }
